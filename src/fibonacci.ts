d0297102f097c3764603a59c6e3daeead81fb0ab
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 09:41:57 +0100

    allow an optional negative recursive approach

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,4 +1,4 @@
-export const computeFibonacciNumber = (position: number | null, recursion: boolean = false): number => {
+export const computeFibonacciNumber = (position: number | null, isRecursive: boolean = false): number => {
 
 
     let notNullPosition = position;
@@ -9,6 +9,8 @@ export const computeFibonacciNumber = (position: number | null, recursion: boole
     if (notNullPosition === 0) {
         return 0;
     }
+    if (isRecursive) return recursiveFibonacci(notNullPosition);
+    
     if (notNullPosition < 0) {
         return computeNegativeFibonacci(notNullPosition);
     }
@@ -19,10 +21,7 @@ export const computeFibonacciNumber = (position: number | null, recursion: boole
         return 1;
     }
 
-    if (recursion) {
-        return recursiveFibonacci(1, 1, notNullPosition - 2);
-    }
-
+    
     let i = 1;
     let j = 1;
 
@@ -36,15 +35,14 @@ export const computeFibonacciNumber = (position: number | null, recursion: boole
     return j;
 };
 
-const recursiveFibonacci = (previous: number, current: number, stepsLeft: number): number => {
-    if (stepsLeft < 0) {
-        return 1;
-    }
-    switch (stepsLeft) {
-        case 0:
-            return current;
-        default:
-            return recursiveFibonacci(current, previous + current, stepsLeft - 1);
+const recursiveFibonacci = (initialPosition: number, left: number = 0, right: number = 1, position?: number): number => {
+    const currentPosition = position ?? initialPosition;
+    if (initialPosition === 0) return 0;
+    if (currentPosition === 0) return left;
+    if (initialPosition > 0) {
+        return recursiveFibonacci(initialPosition, right, left + right, currentPosition - 1);
+    } else {
+        return recursiveFibonacci(initialPosition, right - left, left, currentPosition + 1);
     }
 }
 
